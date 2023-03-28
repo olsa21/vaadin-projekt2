9adb27fc398faa62901cfb4de226a96f12a27842
Oliver Sabelfeld <o.sd@tutanota.de>
2023-03-28 14:47:05 +0200

    Zugangsdaten dynamisch

--- src/main/java/org/vaadin/example/SecurityConfig.java
+++ src/main/java/org/vaadin/example/SecurityConfig.java
@@ -1,52 +1,52 @@
 package org.vaadin.example;
 
-import java.util.Collections;
-
+import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import com.vaadin.flow.spring.security.VaadinWebSecurity;
+import org.vaadin.example.entity.MitarbeiterEntity;
+import org.vaadin.example.service.SpecificationsService;
 import org.vaadin.example.views.LoginPage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends VaadinWebSecurity {
+    private final SpecificationsService service;
 
-    /**
-     * Demo SimpleInMemoryUserDetailsManager, which only provides
-     * two hardcoded in-memory users and their roles.
-     * NOTE: This shouldn't be used in real-world applications.
-     */
-    private static class SimpleInMemoryUserDetailsManager extends InMemoryUserDetailsManager {
-        public SimpleInMemoryUserDetailsManager() {
-            createUser(new User("user",
-                    "{noop}userpass",
-                    Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))
-            ));
-            createUser(new User("admin",
-                    "{noop}userpass",
-                    Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))
-            ));
-        }
+    public SecurityConfig(SpecificationsService service) {
+        this.service = service;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //TODO optional
-        http.authorizeRequests().antMatchers("/images/**").permitAll();
-
+        //http.authorizeHttpRequests().requestMatchers("/images/*.png").permitAll();
         super.configure(http);
-
         setLoginView(http, LoginPage.class);
     }
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        return new SimpleInMemoryUserDetailsManager();
+    public UserDetailsService users() {
+        List<MitarbeiterEntity> mitarbeiterEntities = service.findAllUser("");
+
+        ArrayList<UserDetails> userDetails = new ArrayList<>();
+
+        for (MitarbeiterEntity mitarbeiterEntity : mitarbeiterEntities) {
+            userDetails.add(User.builder()
+                    .username(mitarbeiterEntity.getBenutzername())
+                    .password("{SHA-256}"+mitarbeiterEntity.getPasswort())
+                    .roles("USER")
+                    .build());
+        }
+
+        //return new InMemoryUserDetailsManager(user, admin);
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
